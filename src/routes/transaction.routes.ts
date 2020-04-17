177aa1c9900e561e88ed0ef4b4cf61c8c052cdad
Rodrigo Yoshioka <rodrigo.yoshioka@gmail.com>
2020-04-17 14:38:53 -0300

    Add create transaction routine

--- src/routes/transaction.routes.ts
+++ src/routes/transaction.routes.ts
@@ -1,11 +1,14 @@
 import { Router } from 'express';
 
-// import TransactionsRepository from '../repositories/TransactionsRepository';
-// import CreateTransactionService from '../services/CreateTransactionService';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import CreateTransactionService from '../services/CreateTransactionService';
 
 const transactionRouter = Router();
 
-// const transactionsRepository = new TransactionsRepository();
+const transactionsRepository = new TransactionsRepository();
+const createTransactionService = new CreateTransactionService(
+  transactionsRepository,
+);
 
 transactionRouter.get('/', (request, response) => {
   try {
@@ -18,6 +21,14 @@ transactionRouter.get('/', (request, response) => {
 transactionRouter.post('/', (request, response) => {
   try {
     // TODO
+    const { title, value, type } = request.body;
+    const transaction = createTransactionService.execute({
+      title,
+      value,
+      type,
+    });
+
+    return response.json(transaction);
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
